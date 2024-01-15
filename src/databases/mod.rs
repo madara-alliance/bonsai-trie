315546ddee0f93ae22eebc05f9f8dc4e897673e9
Timothée Delabrouille <timothee.delabrouille@gmail.com>
2024-01-15 16:41:42 +0100

    fix: actually used rocksdb feature flag

--- src/databases/mod.rs
+++ src/databases/mod.rs
@@ -1,6 +1,8 @@
 #![allow(dead_code)]
 mod hashmap_db;
 
+#[cfg(feature = "rocksdb")]
 mod rocks_db;
 
+#[cfg(feature = "rocksdb")]
 pub use rocks_db::{create_rocks_db, RocksDB, RocksDBBatch, RocksDBConfig};
