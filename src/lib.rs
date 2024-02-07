6848af471339c5ba421baa42e76ff4ed19055a76
antiyro <antiyro@gmail.com>
2024-02-07 18:28:57 +0100

    cleaned: cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -109,7 +109,7 @@ mod error;
 /// Definition and basic implementation of an CommitID
 pub mod id;
 
-pub use bonsai_database::{BonsaiDatabase, DBError, BonsaiPersistentDatabase, DatabaseKey};
+pub use bonsai_database::{BonsaiDatabase, BonsaiPersistentDatabase, DBError, DatabaseKey};
 pub use error::BonsaiStorageError;
 pub use trie::merkle_tree::{Membership, ProofNode};
 
