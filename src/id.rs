7f81111512cc459170846d25450654abb870310f
antiyro <antiyro@gmail.com>
2024-03-01 14:28:06 +0100

    added block number support for basic id

--- src/id.rs
+++ src/id.rs
@@ -11,9 +11,27 @@ pub trait Id: hash::Hash + PartialEq + Eq + PartialOrd + Ord + Debug + Copy {
 #[derive(Hash, PartialEq, Eq, PartialOrd, Ord, Debug, Clone, Copy, Default)]
 pub struct BasicId(u64);
 
+impl BasicId {
+    /// Constructor for creating a new `BasicId` from a `u64`.
+    pub fn new(id: u64) -> Self {
+        BasicId(id)
+    }
+
+    /// Converts the ID to a byte vector.
+    pub fn to_bytes(&self) -> Vec<u8> {
+        self.0.to_be_bytes().to_vec()
+    }
+}
+
 impl Id for BasicId {
     fn to_bytes(&self) -> Vec<u8> {
-        self.0.to_be_bytes().to_vec()
+        self.to_bytes()
+    }
+}
+
+impl From<u64> for BasicId {
+    fn from(item: u64) -> Self {
+        BasicId::new(item)
     }
 }
 
@@ -36,12 +54,7 @@ impl BasicIdBuilder {
 
     /// Create a new ID (unique).
     pub fn new_id(&mut self) -> BasicId {
-        let id = BasicId(self.last_id);
         self.last_id = self.last_id.checked_add(1).expect("Id overflow");
-        id
-    }
-
-    pub fn block_number(id: u64) -> BasicId {
-        BasicId(id)
+        BasicId::new(self.last_id - 1)
     }
 }
